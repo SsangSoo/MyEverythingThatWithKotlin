c63552e113bc3ba5eb95fb22c2a6b4de3170be84
SsangSoo <kss10080@naver.com>
2023-12-28 00:06:33 +0900

    At 05 ok

---
+++ atomickotlin/src/DataTypes/Types.kt
@@ -0,0 +1,22 @@
+package DataTypes
+
+fun main() {
+    val whole : Int = 11                        // 11
+    val fractional : Double = 1.4               // 1.4
+    val trueOfFalse : Boolean = true            // true
+    val words : String = "A value"              // A value
+    val character : Char = 'z'                  // z
+    val lines : String = """Triple quotes let       
+                            you have many lines
+                            in your string"""
+//    Triple quotes let
+//    you have many lines
+//    in your string
+
+    println(whole)
+    println(fractional)
+    println(trueOfFalse)
+    println(words)
+    println(character)
+    println(lines)
+}
