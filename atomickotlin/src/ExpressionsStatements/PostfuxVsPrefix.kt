8124a27223dfe1ddb50dd064c2a1426e543b3242
SsangSoo <kss10080@naver.com>
2024-01-11 23:16:06 +0900

    ch 14 OK

---
+++ atomickotlin/src/ExpressionsStatements/PostfuxVsPrefix.kt
@@ -0,0 +1,10 @@
+package ExpressionsStatements
+
+fun main() {
+    var i = 10
+    println(i++)    // 10
+    println(i)      // 11
+    var j = 20
+    println(++j)    // 21
+    println(j)      // 21
+}
---
+++ atomickotlin/src/ExpressionsStatements/UnitReturnType.kt
@@ -0,0 +1,12 @@
+package ExpressionsStatements
+
+fun unitFun() = Unit
+
+fun main() {
+    println(unitFun())
+    val u1: Unit = println(42)
+    println(u1)
+    val u2 = println(0) // 타입 추론
+    println(u2)
+
+}
