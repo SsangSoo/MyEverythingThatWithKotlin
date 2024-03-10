e35ab3d2c07493eb69a96e314c6886c466f28b31
SsangSoo <kss10080@naver.com>
2024-03-10 17:08:20 +0900

    OK

---
+++ atomickotlin/src/sec05/polymorphism/Pet.kt
@@ -0,0 +1,24 @@
+package sec05.polymorphism
+import atomictest.eq
+
+open class Pet {
+    open fun speak() = "Pet"
+}
+
+class Dog : Pet() {
+    override fun speak() = "Bark!"
+
+}
+
+class Cat : Pet() {
+    override fun speak() = "Meow"
+}
+
+fun talk(pet: Pet) = pet.speak()
+
+
+fun main() {
+    talk(Dog()) eq "Bark!"
+    talk(Cat()) eq "Meow"
+
+}
